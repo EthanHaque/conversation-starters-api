60381e792e71d90931df2129fa34e0e6b7013e87
Ethan Haque <43977335+EthanHaque@users.noreply.github.com>
2021-08-18 14:12:57 -0500

    fixing url

--- public/js/question.js
+++ public/js/question.js
@@ -1,7 +1,7 @@
 
 
 async function getRandomQuestion() {
-    const url = "https://conversation-starters-api.herokuapp.com/api/question";
+    const url = "https://conversation-starters-api.herokuapp.com/api/v1/question";
     try {
         const response = await fetch(url);
         const data = await response.json();
